278d0972c4cc6836459f01ee81d72b72c8e240db
Christopher <christopher.mazaudou@gmail.com>
2023-12-04 20:43:20 +0100

    add d04 aoc 2023

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ My solutions doing [AoC](https://adventofcode.com/).
 
 All problems were solved in [Kotlin](https://kotlinlang.org/).
 
-| Day | Problem                                                | Solution                   |
-|-----|--------------------------------------------------------|----------------------------|
-| 1   | [Trebuchet?!](https://adventofcode.com/2023/day/1)     | [Day01.kt](./src/Day01.kt) |
-| 2   | [Cube Conundrum](https://adventofcode.com/2023/day/2)  | [Day02.kt](./src/Day02.kt) |
-| 3   | [Gear Ratios](https://adventofcode.com/2023/day/3)     | [Day03.kt](./src/Day03.kt) |
+| Day | Problem                                               | Solution                   |
+|-----|-------------------------------------------------------|----------------------------|
+| 1   | [Trebuchet?!](https://adventofcode.com/2023/day/1)    | [Day01.kt](./src/Day01.kt) |
+| 2   | [Cube Conundrum](https://adventofcode.com/2023/day/2) | [Day02.kt](./src/Day02.kt) |
+| 3   | [Gear Ratios](https://adventofcode.com/2023/day/3)    | [Day03.kt](./src/Day03.kt) |
+| 4   | [Scratchcards](https://adventofcode.com/2023/day/4)   | [Day04.kt](./src/Day04.kt) |
---
+++ src/Day04.kt
@@ -0,0 +1,46 @@
+import kotlin.math.pow
+
+class Day04(private val lines: List<String>) {
+    fun part1(): Int {
+        return lines.map { Card(it) }
+            .sumOf { it.point }
+    }
+
+    fun part2(): Int {
+        val cardsCount = mutableMapOf<Int, Int>()
+        return lines.map { Card(it) }
+            .mapIndexed { index, card ->
+                val cardToCount = cardsCount.getOrDefault(index, 1)
+                for (j in index + 1..index + card.matchesCount) {
+                    cardsCount[j] = (cardsCount[j] ?: 1) + cardToCount
+                }
+                cardToCount
+            }
+            .sum()
+    }
+}
+
+class Card(input: String) {
+    private val winningsNumber: Set<String>
+    private val scratchNumbers: Set<String>
+    val matchesCount: Int
+    val point: Int
+        get() {
+            return if (matchesCount > 0) 2.0.pow(matchesCount - 1).toInt() else 0
+        }
+
+    init {
+        this.winningsNumber = input.substringAfter(":").split('|').first().windowed(3, 3).toSet()
+        this.scratchNumbers = input.substringAfter(":").split('|').last().windowed(3, 3).toSet()
+        this.matchesCount = winningsNumber.intersect(scratchNumbers).size
+    }
+}
+
+fun main() {
+    val day = Day04(readInput("Day04"))
+    println("Part 1: ${day.part1()}")
+    println("Part 2: ${day.part2()}")
+}
+
+
+
